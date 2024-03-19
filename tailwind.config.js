c5dca3d1976af79f2b3b8eaa4e2bba9e469209be
inaseem <inaseem@protonmail.com>
2024-03-19 15:50:39 +0530

    fix: screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      screens: {
+        sm: '890px',
+        lg: '1200px',
+      },
       colors: {
         white100: 'hsla(0, 0%, 100%, 1)',
         white99: 'hsla(0, 0%, 99%, 1)',
